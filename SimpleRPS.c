0f67982a3496663ccfa74f66b674a1031fa653d9
AnanthanarayanKV <ananthanarayan812@gmail.com>
2024-11-27 20:26:24 +0530

    points system added to simpleRPS

--- SimpleRPS.c
+++ SimpleRPS.c
@@ -7,41 +7,63 @@ int main()
     srand(time(NULL));
 
     int r = rand()%100;
-    int player,i=0;
+    int player,i=0,p = 0,c = 0;
     
-    printf("There will be five rounds...\n");
+    printf("There will be three rounds...\n");
 
     do{
-        printf("\n\nThis is round no:%d...",(i+1));
+        printf("\nThis is round no:%d...",(i+1));
         printf("\nThis is a rock paper scissor game\nSelect one");
         printf("\n\t1 = Rock\n\t2 = Paper\n\t3 = Scissor\n:");
         scanf("%d",&player);
 
-        if((r<33)&&(player==2))
+        if((r<33)&&(player==2)){
+            printf("\nComputer selected Rock");
             printf("\nPlayer won!\n");
+            p++;
 
-        else if(((r>=33)&&(r<66))&&(player==3))
+        }
+        else if(((r>=33)&&(r<66))&&(player==3)){
+            printf("\nComputer selected Paper");
             printf("\nPlayer won!\n");
+            p++;
 
-        else if((r>=66)&&(player==1))
+        }
+
+        else if((r>=66)&&(player==1)){
+            printf("\nComputer selected Scissor");
             printf("\nPlayer won!\n");
+            p++;
+        }
 
         
-        else if((r<33)&&(player==3))
+        else if((r<33)&&(player==3)){
+            printf("\nComputer selected Rock");
             printf("\nComputer won!\n");
+            c++;
 
-        else if(((r>=33)&&(r<66))&&(player==1))
-            printf("\nComputer won!\n");
+        }
 
-        else if((r>=66)&&(player==2))
+        else if(((r>=33)&&(r<66))&&(player==1)){
+            printf("\nComputer selected Paper");
             printf("\nComputer won!\n");
+            c++;
+        }
+
+        else if((r>=66)&&(player==2)){
+            printf("\nComputer selected Scissor");
+            printf("\nComputer won!\n");
+            c++;
+        }
 
         else
             printf("\nIts a tie....\n");
 
 
         i++;
-    }while (i<5);
+    }while (i<3);
+
+    printf("\n Game ended and player won %d times and computer won %d times",p,c);
     
 
     return 0;
